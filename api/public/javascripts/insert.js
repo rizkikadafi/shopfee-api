62fe72e5deda39a77aa4605888ed673ac6e7a6cb
rizkikadafi <rizkikadafi11@gmail.com>
2023-11-21 09:50:43 +0700

    edit insert

--- api/public/javascripts/insert.js
+++ api/public/javascripts/insert.js
@@ -4,18 +4,36 @@ document.addEventListener('DOMContentLoaded', function () {
   form.addEventListener('submit', async function (event) {
     event.preventDefault();
 
-    const response = await fetch('/insert', {
-      method: 'POST',
-      body: formData,
-    });
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const description = formData.get('description');
+    const rating = formData.get('rating');
+    const small_price = formData.get('small_price');
+    const medium_price = formData.get('medium_price');
+    const large_price = formData.get('large_price');
 
-    const data = await response.json();
+    try {
+      const response = await fetch('/insert', {
+        method: 'POST',
+        body: JSON.stringify({ 
+          name, 
+          description,
+          rating,
+          small_price,
+          medium_price,
+          large_price
+        }),
+      });
 
-    if(data.success) {
-      alert(data.message);
-      window.location.href = '/';
-    } else {
-      alert(data.message);
+      const data = await response.json();
+  
+      if (data.success) {
+        window.location.href = '/'; 
+      } else {
+        alert(data.message);
+      }
+    } catch (error) {
+      alert('Terjadi kesalahan saat login');
     }
   });
 });
